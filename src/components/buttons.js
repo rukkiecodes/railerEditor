9cb3ecaae93554e614b0cc260de05bfd32ec7918
rukkiecodes <rukkiecodes@gmail.com>
2021-10-17 23:22:04 +0100

    moved blockes out

---
+++ src/components/buttons.js
@@ -0,0 +1,90 @@
+export default [
+  {
+    id: "visibility",
+    active: false, // active by default
+    className: "btn-toggle-borders",
+    label: `<span class="mdi mdi-border-all-variant"></span>`,
+    command: "sw-visibility", // Built-in command
+    attributes: {
+      title: "Border visibility",
+      class: "panel-buttons",
+    },
+  },
+  {
+    id: "export",
+    className: "btn-open-export",
+    label: `<span class="mdi mdi-code-tags"></span>`,
+    command: "export-template",
+    context: "export-template", // For grouping context of buttons from the same panel
+    attributes: {
+      title: "Export template",
+      class: "panel-buttons",
+    },
+  },
+  {
+    id: "show-json",
+    className: "btn-show-json",
+    label: `<span class="mdi mdi-code-json"></span>`,
+    context: "show-json",
+    command: "show-json",
+    attributes: {
+      title: "Show JSON",
+      class: "panel-buttons",
+    },
+  },
+  {
+    id: "undo",
+    className: "btn-undo",
+    label: `<span class="mdi mdi-undo"></span>`,
+    context: "undo",
+    command: "undo",
+    attributes: {
+      title: "Undo",
+      class: "panel-buttons",
+    },
+  },
+  {
+    id: "redo",
+    className: "btn-redo",
+    label: `<span class="mdi mdi-redo"></span>`,
+    context: "redo",
+    command: "redo",
+    attributes: {
+      title: "Redo",
+      class: "panel-buttons",
+    },
+  },
+  {
+    id: "fullscreen",
+    className: "btn-fullscreen",
+    label: `<span class="mdi mdi-fullscreen"></span>`,
+    context: "fullscreen",
+    command: "fullscreen",
+    attributes: {
+      title: "Fullscreen",
+      class: "panel-buttons",
+    },
+  },
+  {
+    id: "import",
+    className: "btn-import",
+    label: `<span class="mdi mdi-download"></span>`,
+    context: "import",
+    command: "import",
+    attributes: {
+      title: "Import",
+      class: "panel-buttons",
+    },
+  },
+  {
+    id: "clear-canvas",
+    className: "btn-clear-canvas",
+    label: `<span class="mdi mdi-trash-can"></span>`,
+    context: "clear-canvas",
+    command: "clear-canvas",
+    attributes: {
+      title: "Clear canvas",
+      class: "panel-buttons",
+    },
+  },
+]
--- src/components/editorConfig.js
+++ src/components/editorConfig.js
@@ -1,3 +1,5 @@
+import buttons from "./buttons"
+
 export default (editor) => {
   editor.Panels.addPanel({
     id: "panel-top",
@@ -7,114 +9,7 @@ export default (editor) => {
   editor.Panels.addPanel({
     id: "basic-actions",
     el: ".panel__basic-actions",
-    buttons: [
-      {
-        id: "visibility",
-        active: false, // active by default
-        className: "btn-toggle-borders",
-        label: `<span class="mdi mdi-border-all-variant"></span>`,
-        command: "sw-visibility", // Built-in command
-        attributes: {
-          title: "Border visibility",
-          class: "panel-buttons",
-        },
-      },
-      {
-        id: "export",
-        className: "btn-open-export",
-        label: `<span class="mdi mdi-code-tags"></span>`,
-        command: "export-template",
-        context: "export-template", // For grouping context of buttons from the same panel
-        attributes: {
-          title: "Export template",
-          class: "panel-buttons",
-        },
-      },
-      {
-        id: "show-json",
-        className: "btn-show-json",
-        label: `<span class="mdi mdi-code-json"></span>`,
-        context: "show-json",
-        attributes: {
-          title: "Show JSON",
-          class: "panel-buttons",
-        },
-        command(editor) {
-          editor.Modal.setTitle("Components JSON")
-            .setContent(
-              `<textarea style="width:100%; height: 250px;">
-            ${JSON.stringify(editor.getComponents())}
-          </textarea>`
-            )
-            .open()
-        },
-      },
-      {
-        id: "undo",
-        className: "btn-undo",
-        label: `<span class="mdi mdi-undo"></span>`,
-        context: "undo",
-        attributes: {
-          title: "Undo",
-          class: "panel-buttons",
-        },
-        command(editor) {
-          console.log(editor)
-        },
-      },
-      {
-        id: "redo",
-        className: "btn-redo",
-        label: `<span class="mdi mdi-redo"></span>`,
-        context: "redo",
-        attributes: {
-          title: "Redo",
-          class: "panel-buttons",
-        },
-        command(editor) {
-          console.log(editor)
-        },
-      },
-      {
-        id: "fullscreen",
-        className: "btn-fullscreen",
-        label: `<span class="mdi mdi-fullscreen"></span>`,
-        context: "fullscreen",
-        attributes: {
-          title: "Fullscreen",
-          class: "panel-buttons",
-        },
-        command(editor) {
-          console.log(editor)
-        },
-      },
-      {
-        id: "import",
-        className: "btn-import",
-        label: `<span class="mdi mdi-download"></span>`,
-        context: "import",
-        attributes: {
-          title: "Import",
-          class: "panel-buttons",
-        },
-        command(editor) {
-          console.log(editor)
-        },
-      },
-      {
-        id: "clear-canvas",
-        className: "btn-clear-canvas",
-        label: `<span class="mdi mdi-trash-can"></span>`,
-        context: "clear-canvas",
-        attributes: {
-          title: "Clear canvas",
-          class: "panel-buttons",
-        },
-        command(editor) {
-          console.log(editor)
-        },
-      },
-    ],
+    buttons,
   })
 
   // Commands
@@ -126,5 +21,44 @@ export default (editor) => {
   })
   editor.Commands.add("set-device-mobile", {
     run: (editor) => editor.setDevice("Mobile"),
+  })
+  editor.Commands.add("set-device-mobile", {
+    run: (editor) => editor.setDevice("Mobile"),
+  })
+  editor.Commands.add("show-json", {
+    run: (editor) => {
+      editor.Modal.setTitle("Components JSON")
+        .setContent(
+          `<textarea style="width:100%; height: 250px; color: #fff">
+            ${JSON.stringify(editor.getComponents())}
+          </textarea>`
+        )
+        .open()
+    },
+  })
+  editor.Commands.add("undo", {
+    run: (editor) => {
+      console.log(editor)
+    },
+  })
+  editor.Commands.add("redo", {
+    run: (editor) => {
+      console.log(editor)
+    },
+  })
+  editor.Commands.add("fullscreen", {
+    run: (editor) => {
+      console.log(editor)
+    },
+  })
+  editor.Commands.add("import", {
+    run: (editor) => {
+      console.log(editor)
+    },
+  })
+  editor.Commands.add("clear-canvas", {
+    run: (editor) => {
+      console.log(editor)
+    },
   })
 }
