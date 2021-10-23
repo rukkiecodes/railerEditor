77714de3780b9ece3d684c9fa171ce253c097634
Terry Amagboro <rukkiecodes@gmail.com>
2021-10-24 00:04:57 +0100

    added css code editor

--- src/components/CodeEditor.vue
+++ src/components/CodeEditor.vue
@@ -1,7 +1,7 @@
 <template>
   <v-bottom-sheet v-model="editor.dialog" dark persistent>
-    <v-sheet class="text-center" min-height="300">
-      <v-toolbar dense flat>
+    <v-sheet class="text-center" min-height="300" color="#1c1e31">
+      <v-toolbar dense flat color="#1c1e31">
         <v-spacer></v-spacer>
         <v-btn icon @click="editor.dialog = false">
           <v-icon>mdi-close</v-icon>
@@ -9,17 +9,25 @@
       </v-toolbar>
       <v-row no-gutters justify="space-between" align="start">
         <v-col cols="6">
-          <v-sheet min-height="500" color="white" class="htmlEditorContainer">
+          <v-sheet min-height="500" color="transparent" class="htmlEditorContainer">
             <div
-              :style="{ width: editorWidth + 'px', height: editorHeight + 'px' }"
+              :style="{
+                width: editorWidth + 'px',
+                height: editorHeight + 'px',
+              }"
               id="aceHtml"
-            >
-            </div>
+            ></div>
           </v-sheet>
         </v-col>
         <v-col cols="6">
-          <v-sheet min-height="500" color="white">
-            <div id="aceCss"></div>
+          <v-sheet min-height="500" color="white" class="cssEditorContainer">
+            <div
+              :style="{
+                width: editorWidth + 'px',
+                height: editorHeight + 'px',
+              }"
+              id="aceCss"
+            ></div>
           </v-sheet>
         </v-col>
       </v-row>
@@ -28,7 +36,9 @@
 </template>
 
 <script>
+// @ts-nocheck
 import htmlCodeEditor from "./htmlCodeEditor.js"
+import csslCodeEditor from "./csslCodeEditor.js"
 import { mapState } from "vuex"
 export default {
   data: () => ({
@@ -38,11 +48,13 @@ export default {
   mounted() {
     this.$nextTick(() => {
       const htmlEditorContainer = document.querySelector(".htmlEditorContainer")
+      const cssEditorContainer = document.querySelector(".cssEditorContainer")
 
-      if (htmlEditorContainer) {
+      if (htmlEditorContainer && cssEditorContainer) {
         this.editorWidth = htmlEditorContainer.offsetWidth
         this.editorHeight = htmlEditorContainer.offsetHeight
         htmlCodeEditor()
+        csslCodeEditor()
       }
     })
   },
