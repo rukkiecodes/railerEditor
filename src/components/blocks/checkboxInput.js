3051d5f361bc2552d47e3c4d3494d2eb6c5a40f9
Terry Amagboro <rukkiecodes@gmail.com>
2021-10-25 22:23:54 +0100

    modified check input block

--- src/components/blocks/checkboxInput.js
+++ src/components/blocks/checkboxInput.js
@@ -6,24 +6,11 @@ export default {
   label: `<div class="styleBlock"><img style="width: 80%" src="${checkboxInput}"><span>Checkbox input</span></div>`,
   attributes: { class: "checkboxInput_block" },
   content: `
-      <div class="form-group">
-        <input type="checkbox" name="checkbox">
-        <label for="checkbox">checkbox</label>
-      </div>
-      <style>
-        * {
-          box-sizing: border-box;
-          font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-        }
-
-        .form-group {
-          display: flex;
-          align-items: center;
-        }
-
-        label {
-          font-size: .9em;
-        }
-      </style>
-      `,
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+      <label class="form-check-label" for="flexCheckDefault">
+        Default checkbox
+      </label>
+    </div>
+  `,
 }
--- src/components/blocks/radioInput.js
+++ src/components/blocks/radioInput.js
@@ -6,25 +6,11 @@ export default {
   label: `<div class="styleBlock"><img style="width: 80%" src="${radioInput}"><span>Radio input</span></div>`,
   attributes: { class: "radioInput_block" },
   content: `
-      <div class="form-group">
-        <input type="radio" />
-        <label>Label</label>
-      </div>
-      <style>
-        * {
-          box-sizing: border-box;
-          font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-        }
-
-        .form-group {
-          display: flex;
-          align-items: center;
-        }
-        
-        label {
-          font-size: .9em;
-        }
-      </style>
-      </style>
-      `,
+    <div class="form-check">
+      <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+      <label class="form-check-label" for="flexRadioDefault1">
+        Default radio
+      </label>
+    </div>
+  `,
 }
