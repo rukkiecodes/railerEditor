9d08a15d087feacc3979fe465a263ead91bdbc0e
Terry Amagboro <rukkiecodes@gmail.com>
2021-10-25 22:13:22 +0100

    modified password input block

--- src/components/blocks/passwordInput.js
+++ src/components/blocks/passwordInput.js
@@ -6,37 +6,10 @@ export default {
   label: `<div class="styleBlock"><img style="width: 80%" src="${passwordInput}"><span>Password input</span></div>`,
   attributes: { class: "passwordInput_block" },
   content: `
-      <div class="form-group">
-        <label for="password" class="label">Password</label>
-        <input type="password" class="input" name="email" placeholder="Password input" />
-      </div>
-      <style>
-        * {
-          box-sizing: border-box;
-          font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-        }
-
-        .input {
-          width: 100%;
-          height: 3em;
-          margin-bottom: 1.5em;
-          padding: 7px 10px;
-          border-radius: .4em;
-          color: rgba(0, 0, 0, 0.8);
-          background-color: #fff;
-          outline: none;
-          border: 2px solid transparent;
-          transition: .2s ease-in-out;
-        }
-
-        .input:focus {
-          border: 2px solid #4169e15d
-        }
-
-        .label {
-          font-size: .9em;
-          display: block;
-        }
-      </style>
-      `,
+    <label for="inputPassword5" class="form-label">Password</label>
+    <input type="password" id="inputPassword5" class="form-control" aria-describedby="passwordHelpBlock">
+    <div id="passwordHelpBlock" class="form-text">
+      Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
+    </div>
+  `,
 }
