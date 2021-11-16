3999fbee8f1f289338d5b03ac72ee23ac31f88af
Terry Amagboro <rukkiecodes@gmail.com>
2021-11-16 22:29:51 +0100

    add extra blocks

--- src/components/editor/editor.js
+++ src/components/editor/editor.js
@@ -5,6 +5,12 @@ import "./assets/grapes.css"
 import blocksPlugin from "./blocks/basic"
 import formPlug from "./blocks/form"
 import gjsnavbar from "./blocks/navbar"
+import gjscomponentcountdown from "./blocks/countdown"
+import typedPlugin from "./blocks/typed"
+import grapesjsloryslider from './blocks/slider'
+import grapesjscustomcode from "./blocks/costomCode"
+import grapesjsTabs from "./blocks/tabs"
+import grapesjstooltip from "./blocks/tooltip"
 
 import editorConfig from "./editorConfig"
 import blocks from "./blocks"
@@ -30,17 +36,27 @@ export default () => {
     width: "auto",
     storageManager: false,
 
-    plugins: [blocksPlugin, formPlug, gjsnavbar],
+    plugins: [
+      blocksPlugin,
+      formPlug,
+      gjsnavbar,
+      gjscomponentcountdown,
+      typedPlugin,
+      grapesjsloryslider,
+      grapesjscustomcode,
+      grapesjsTabs,
+      grapesjstooltip,
+    ],
     pluginsOpts: {
-      blocksPlugin: {
-        /* options */
-      },
-      formPlug: {
-        /* options */
-      },
-      gjsnavbar: {
-        /* options */
-      },
+      blocksPlugin: {},
+      formPlug: {},
+      gjsnavbar: {},
+      gjscomponentcountdown: {},
+      typedPlugin: {},
+      grapesjsloryslider: {},
+      grapesjscustomcode: {},
+      grapesjsTabs: {},
+      grapesjstooltip: {},
     },
 
     blockManager,
