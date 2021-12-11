c9fe7612d440dead2c3520854b9d21ba25d4525b
Terry Amagboro <rukkiecodes@gmail.com>
2021-12-11 23:32:23 +0100

    add tabs for blocks

--- src/components/editor/Editor.vue
+++ src/components/editor/Editor.vue
@@ -68,7 +68,36 @@
       </div>
       <div class="editor-row">
         <div class="panel_left_container" :style="{ width: panelWidth + 'px' }">
-          <div class="blocks-container"></div>
+          <!-- <div class="blocks-container"></div> -->
+          <v-card>
+            <v-tabs
+              v-model="tab"
+              background-color="transparent"
+              color="basil"
+              grow
+            >
+              <v-tab>
+                <v-icon>mdi-shape-plus</v-icon>
+              </v-tab>
+              
+              <v-tab>
+                <v-icon>mdi-rocket-launch</v-icon>
+              </v-tab>
+            </v-tabs>
+
+            <v-tabs-items v-model="tab">
+              <v-tab-item>
+                <v-card color="white" flat>
+                  <div class="blocks-container"></div>
+                </v-card>
+              </v-tab-item>
+              <v-tab-item>
+                <v-card color="white" flat>
+                  <v-card-text>{{ text }}</v-card-text>
+                </v-card>
+              </v-tab-item>
+            </v-tabs-items>
+          </v-card>
         </div>
         <div class="editor-canvas">
           <div id="gjs"></div>
@@ -104,6 +133,10 @@ export default {
     panelWidth: 250,
     panel: true,
     panelDisplay: "",
+    tab: null,
+    items: ["mdi-shape-plus", "mdi-rocket-launch"],
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
   }),
 
   components: {
