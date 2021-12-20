74099342c69c9bdb7e36a1ac31c67898620b3ec5
Terry Amagboro <rukkiecodes@gmail.com>
2021-12-20 20:48:51 +0100

    edit trash

--- src/components/editor/managers.js
+++ src/components/editor/managers.js
@@ -468,9 +468,20 @@ const panels = {
         {
           id: "canvas-clear",
           className: "fa fa-trash",
-          command: (e) => {
-            const confirmDelete = confirm("Are you sure to clean the canvas?")
-            if (confirmDelete) e.runCommand("core:canvas-clear")
+          command: (editor, sender) => {
+            if (sender) sender.set("active", false)
+            if (confirm("Are you sure to clean the canvas?")) {
+              editor.runCommand("core:canvas-clear")
+              setTimeout(function() {
+                localStorage.setItem("gjs-assets", "")
+                localStorage.setItem("gjs-components", "")
+                localStorage.setItem("gjs-html", "")
+                localStorage.setItem("gjs-css", "")
+                localStorage.setItem("gjs-styles", "")
+                localStorage.removeItem("gjs-scripts", "")
+                localStorage.removeItem("gram-dependencies", "")
+              }, 0)
+            }
           },
         },
       ],
